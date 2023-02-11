cffe281ef4ff4dcc077e3e3c8b58f98461fa16ac
HuangHuangChia <72678149+HuangHuangChia@users.noreply.github.com>
2023-02-11 08:43:26 +0800

    Add files via upload

---
+++ LineNotify_web_Eng.bat
@@ -0,0 +1,12 @@
+cd c:\Task\webcheck
+call c:\Task\webcheck\updateEnglishVersion.bat
+c:\Task\webcheck\typc_eng.py
+timeout /t 10 >nul
+chcp 65001
+IF EXIST "c:\Task\webcheck\TYPC_Eng_info.txt" (
+c:\Task\LineNotify\LineNotify_general.exe "0xQH1k7AuD1CERF9Hy12XNv8MnTOE8V6MwitbfdJHe9" "c:\Task\webcheck\Typc_Eng_info.txt"
+) ELSE (
+ECHO "沒執行linenotify"
+)
+rem c:\LineNotify\LineNotify_general.exe "lj7BBFFqnWuoezANZEd5jD3NJu9dZkJFua3Cn4wIz2q" "c:\webcheck\TYPC_Chn_info.txt"
+rem timeout /t 100 >nul
